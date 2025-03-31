d20de16b7996cb38f691289b4a9b7fb790d76873
nayzakk <132763972+nayzakk@users.noreply.github.com>
2025-03-31 15:25:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 def run_headless():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)  # Run in headless mode
+        browser = p.chromium.launch(headless=False)  # Run in headless mode
         page = browser.new_page()
         page.goto("https://google.com")
         print(page.title())  # examplemplelehe page title
